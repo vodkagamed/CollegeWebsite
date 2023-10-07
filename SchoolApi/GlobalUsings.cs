4fc2199a03f058506023b5990a84d42dbc6908e8
mohamed hassan <mohamed2122418@gmail.com>
2023-10-07 18:06:26 +0300

    merge completed

--- SchoolApi/GlobalUsings.cs
+++ SchoolApi/GlobalUsings.cs
@@ -1,9 +1,12 @@
 ﻿global using SchoolApi.Repos;
-global using SchoolApi.Controllers;
 global using SchoolApi.Data;
 global using Microsoft.AspNetCore.Mvc;
 global using SchoolWebsite.shared.Models;
 global using SchoolWebsite.shared;
 global using System.Collections.Concurrent;
 global using System.Text.Json;
+<<<<<<< HEAD
 global using System.Text.RegularExpressions;
+=======
+global using System.Text.RegularExpressions;
+>>>>>>> SW0.3
--- SchoolWebsite.Client/GlobalUsing.cs
+++ SchoolWebsite.Client/GlobalUsing.cs
@@ -1,4 +1,1 @@
 ﻿global using SchoolWebsite.Client.Services;
-global using SchoolWebsite.shared;
-global using SchoolWebsite.Client.Components;
-global using SchoolWebsite.Client;
